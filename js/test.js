c00d14b14ccfeb0d0cb18986184a66b1ae87cacc
Michail <Gismanus@gmail.com>
2022-10-11 19:26:19 +0300

    new test

--- js/test.js
+++ js/test.js
@@ -1,1 +1,17 @@
-console.log('Hello');
+let str = 'Hello';
+
+function changeString(str) {
+    let newStr = '';
+    for (let i = 0; i < str.length; i++) {
+        if (i != 2) {
+            newStr += str[i];
+            console.log(str[i]);
+
+        } else {
+            newStr += 'w';
+            console.log(newStr[i]);
+        }
+    }
+}
+
+changeString(str);
