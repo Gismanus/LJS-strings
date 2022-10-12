9cc388277f4cc47593d87e922b0a8b572d68851d
Michail <Gismanus@gmail.com>
2022-10-12 12:44:09 +0300

    checkMatches

--- js/checkMatch.js
+++ js/checkMatch.js
@@ -1,5 +1,12 @@
 let str = 'Hello';
 
-if(str.indexOf('Hello') != -1){
-    console.log('Match is there!')
-}
+// if(str.indexOf('Hello') != -1){
+//     console.log('Match is there!')
+// }
+
+// console.log(str.includes('Hell'));
+// console.log(str.includes('Hell', 2));
+
+console.log(str.startsWith('He'));
+console.log(str.endsWith('He'));
+console.log(str.endsWith('llo'));
