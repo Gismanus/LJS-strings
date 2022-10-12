56248b53e898a82ae2472e87074aa78007fd1865
Michail <Gismanus@gmail.com>
2022-10-12 17:21:45 +0300

    checkSpam

--- js/tasks.js
+++ js/tasks.js
@@ -1,6 +1,23 @@
-function ucFirst(str) {
+// 1
+/* function ucFirst(str) {
     if(!str) return str;
     return str[0].toUpperCase() + str.slice(1);
 }
 
-console.log(ucFirst(prompt('Enter word', '')));
+console.log(ucFirst(prompt('Enter word', ''))); */
+
+// 2
+
+function checkSpam(str) {
+
+    if (str.toLowerCase().includes
+    ('viagra') || str.toLowerCase().includes
+    ('xxx')) {
+        return true;
+    }
+    return false;
+}
+
+let string = prompt('Enter text', '');
+
+console.log(checkSpam(string));
